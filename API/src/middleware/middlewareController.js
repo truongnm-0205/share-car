0d332cd28cfb9005cc749343f14b319b3bccb5fe
nktoan1905 <nktoan1905@gmail.com>
2022-11-29 01:49:22 +0700

    Update middleare

--- API/src/middleware/middlewareController.js
+++ API/src/middleware/middlewareController.js
@@ -7,13 +7,13 @@ const middlewareController = {
 			const accessToken = token.split(' ')[1];
 			jwt.verify(accessToken, process.env.JWT_ACCESS_KEY, (err, user) => {
 				if (err) {
-					return res.status(403).json('Token is not valid');
+					return res.status(403).json({ status: 'Token is not valid' });
 				}
 				req.user = user;
 				next();
 			});
 		} else {
-			return res.status(401).json("You're not authenticated");
+			return res.status(401).json({ status: "You're not authenticated" });
 		}
 	},
 };
