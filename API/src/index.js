1ee0d01fdc6657b8a65cac96585c68b111047430
nktoan1905 <nktoan1905@gmail.com>
2022-12-12 20:04:01 +0700

    a

--- API/src/index.js
+++ API/src/index.js
@@ -26,16 +26,16 @@ const app = express();
 // };
 
 app.use(cors());
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method',
-	);
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header(
+// 		'Access-Control-Allow-Headers',
+// 		'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method',
+// 	);
+// 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+// 	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+// 	next();
+// });
 app.use(cookieParser());
 app.use(express.json());
 
